812977d01bff8c8bd9a628e983fb7600128b33e9
Randel Jiao <randeljiao@gmail.com>
2020-09-10 21:04:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,17 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :color, :page_count, :genre
+  attr_reader :brand
   
+   def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
   
 end
   
