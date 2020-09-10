cc8eaf7895250e55b8a4194543c66e5aa74bac2b
Randel Jiao <randeljiao@gmail.com>
2020-09-10 21:03:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,12 @@
 class Book
-  
+    attr_accessor :author, :page_count, :genre
+    
   def initialized(title) 
     @title = title
   end
   
   def title
     @title
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genere = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
