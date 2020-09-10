242e9412cccf91b336106229965266b7e48e9a2e
Randel Jiao <randeljiao@gmail.com>
2020-09-10 21:02:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,6 +5,35 @@ class Book
   end
   
   def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genere = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    
   end
 
 end
